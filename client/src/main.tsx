ecc0cdd6227a0d435d0d5311e119fabe578eab61
Trinity <132912921+casmonster@users.noreply.github.com>
2025-04-09 11:20:59 +0200

    Update main.tsx file
    useContext are wrapped by their respective context providers.
    
    Signed-off-by: Trinity <132912921+casmonster@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CartProvider } from './context/CartContext';
+import { WishlistProvider } from './context/WishlistContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CartProvider>
+      <WishlistProvider>
+        <App />
+      </WishlistProvider>
+    </CartProvider>
+  </React.StrictMode>
+);
+
 createRoot(document.getElementById("root")!).render(<App />);
