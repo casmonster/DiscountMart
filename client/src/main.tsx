e165fa5c39a21fc64dc3312a14c330d30878e7b3
Trinity <132912921+casmonster@users.noreply.github.com>
2025-04-09 12:34:12 +0200

    Update main.tsx file
     CartProvider and other providers are wrapping the App component.
    
    Signed-off-by: Trinity <132912921+casmonster@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,8 @@ import ReactDOM from 'react-dom';
 import { CartProvider } from './context/CartContext';
 import { WishlistProvider } from './context/WishlistContext';
 
+console.log('Wrapping App with providers');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
