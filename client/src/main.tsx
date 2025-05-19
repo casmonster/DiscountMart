7b4229d0eda67a2cd9585be10389413cbd063a8b
trinity <mugabetrinity@gmail.com>
2025-05-19 20:04:12 +0200

    Created input.css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./input.css";
 import { Toaster } from "sonner";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
