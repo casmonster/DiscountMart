18957b0f98c327fa87ca887ead5cb330b83a4c45
trinity <mugabetrinity@gmail.com>
2025-05-19 20:11:16 +0200

    removed import "./index.css"; from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import "./input.css";
 import { Toaster } from "sonner";
 import { ErrorBoundary } from "./components/ErrorBoundary";
