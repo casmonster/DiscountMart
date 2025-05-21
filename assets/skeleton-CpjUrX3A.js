13a0a89111d1907cf8c2c5a841fad9ccf8017d8c
trinity <mugabetrinity@gmail.com>
2025-05-21 18:21:55 +0200

    Updates

---
+++ assets/skeleton-CpjUrX3A.js
@@ -0,0 +1,2 @@
+import{j as r}from"./vendor-0nbqjrIh.js";import{t as n,a as s}from"./index-BsuyrIUm.js";function o(...t){return n(s(t))}function u({className:t,...e}){return r.jsx("div",{className:o("animate-pulse rounded-md bg-muted",t),...e})}export{u as S};
+//# sourceMappingURL=skeleton-CpjUrX3A.js.map
